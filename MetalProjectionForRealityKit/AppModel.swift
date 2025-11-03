7da868542ffb0b39d3745ebf9647ff89a2f3729e
banjun <banjun@gmail.com>
2025-11-03 19:01:07 +0900

    Initial Commit

---
+++ MetalProjectionForRealityKit/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  MetalProjectionForRealityKit
+//
+//  Created by banjun on R 7/11/03.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
